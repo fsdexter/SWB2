4a06ead5e190b294c8e1d9c403f0e3febea1a297
Felipe <68749799+fsdexter@users.noreply.github.com>
2021-07-12 12:21:39 +0200

    continue tentando

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -22,13 +22,13 @@ const injectContext = PassedComponent => {
 		);
 
 		useEffect(() => {
-			/**
-			 * EDIT THIS!
-			 * This function is the equivalent to "window.onLoad", it only runs once on the entire application lifetime
-			 * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
-			 * store, instead use actions, like this:
-			 **/
-			state.actions.getMessage(); // <---- calling this function from the flux.js actions
+			state.actions.getDataPeople();
+			state.actions.getDataPlanets();
+			state.actions.getDataSpecies();
+			state.actions.getDataStarships();
+			state.actions.getDataVehicles();
+			//Para poder recoger los datos guardados en el local storage al recargar la página
+			state.actions.getFavourites();
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,46 +1,211 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			dataPeople: [],
+			dataPlanets: [],
+			dataSpecies: [],
+			dataStarships: [],
+			dataVehicles: [],
+			detailsPeople: {},
+			detailsPanet: {},
+			detailsSpecie: {},
+			detailsStarship: {},
+			detailsVehicle: {},
+			favourites: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getDataPeople: async () => {
+				try {
+					let response = await fetch("https://www.swapi.tech/api/people");
+
+					const data = await response.json();
+
+					//Para poder meter el item como favorito
+
+					let formattedCharacters = data.results.map(item => {
+						return { ...item, favorite: false };
+					});
+
+					setStore({ dataPeople: formattedCharacters });
+				} catch (e) {
+					console.error(`error from database -- ${e}`);
+				}
 			},
 
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			getDataPlanets: async () => {
+				try {
+					let response = await fetch("https://www.swapi.tech/api/planets");
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+					const data = await response.json();
+
+					let formattedPlanets = data.results.map(item => {
+						return { ...item, favorite: false };
+					});
+
+					setStore({ dataPlanets: formattedPlanets });
+				} catch (e) {
+					console.error(`error from database -- ${e}`);
+				}
+			},
+
+			getDataSpecies: async () => {
+				try {
+					let response = await fetch("https://www.swapi.tech/api/species");
+
+					const data = await response.json();
+
+					let formattedSpecies = data.results.map(item => {
+						return { ...item, favorite: false };
+					});
+
+					setStore({ dataSpecies: formattedSpecies });
+				} catch (error) {
+					console.error(`error from database -- ${error}`);
+				}
+			},
+
+			getDataStarships: async () => {
+				try {
+					let response = await fetch("https://www.swapi.tech/api/starships");
+
+					const data = await response.json();
+
+					let formattedStarships = data.results.map(item => {
+						return { ...item, favorite: false };
+					});
+
+					setStore({ dataStarships: formattedStarships });
+				} catch (error) {
+					console.error(`error from database -- ${error}`);
+				}
+			},
+
+			getDataVehicles: async () => {
+				try {
+					let response = await fetch("https://www.swapi.tech/api/vehicles");
+
+					const data = await response.json();
+
+					let formattedVehicles = data.results.map(item => {
+						return { ...item, favorite: false };
+					});
+
+					setStore({ dataVehicles: formattedVehicles });
+				} catch (error) {
+					console.error(`error from database -- ${error}`);
+				}
+			},
+
+			getDetailsPeople: async id => {
+				try {
+					let response = await fetch(`https://www.swapi.tech/api/people/${id}`);
+
+					const data = await response.json();
+
+					let data_DetailsPeople = data.result;
+
+					//Da un objecto
+					setStore({ detailsPeople: data_DetailsPeople });
+				} catch (e) {
+					console.error(`error from database -- ${e}`);
+				}
+			},
+
+			getDetailsPlanet: async id => {
+				try {
+					let response = await fetch(`https://www.swapi.tech/api/planets/${id}`);
+
+					const data = await response.json();
+
+					let data_DetailsPlanet = data.result;
+
+					//Da un objecto
+					setStore({ detailsPanet: data_DetailsPlanet });
+				} catch (e) {
+					console.error(`error from database -- ${e}`);
+				}
+			},
+
+			getDetailsSpecie: async id => {
+				try {
+					let response = await fetch(`https://www.swapi.tech/api/species/${id}`);
+
+					const data = await response.json();
+
+					let data_DetailsSpecie = data.result;
+
+					//Da un objecto
+					setStore({ detailsSpecie: data_DetailsSpecie });
+				} catch (e) {
+					console.error(`error from database -- ${e}`);
+				}
+			},
+
+			getDetailsStarship: async id => {
+				try {
+					let response = await fetch(`https://www.swapi.tech/api/starships/${id}`);
+
+					const data = await response.json();
+
+					let data_DetailsStarship = data.result;
+
+					//Da un objecto
+					setStore({ detailsStarship: data_DetailsStarship });
+				} catch (e) {
+					console.error(`error from database -- ${e}`);
+				}
+			},
+
+			getDetailsVehicle: async id => {
+				try {
+					let response = await fetch(`https://www.swapi.tech/api/vehicles/${id}`);
+
+					const data = await response.json();
+
+					let data_DetailsVehicle = data.result;
+
+					//Da un objecto
+					setStore({ detailsVehicle: data_DetailsVehicle });
+				} catch (e) {
+					console.error(`error from database -- ${e}`);
+				}
+			},
+
+			//Para poder añadir a la lista de favoritos todas las categorías
+			//no es posible usar el id, pq el id se repite en distintas categorías
+
+			addFavourite: (dataArr, itemUrl, favouritesArr) => {
+				dataArr.map(item => {
+					if (item.url === itemUrl) {
+						if (favouritesArr.length === 0) {
+							item.favorite = true;
+							setStore({ favourites: [...favouritesArr, item] });
+						} else {
+							//NO REPETIR ELEMENTO EN FAVORITOS
+							if (!favouritesArr.some(item => item.url === itemUrl)) {
+								item.favorite = true;
+								setStore({ favourites: [...favouritesArr, item] });
+							}
+						}
+					}
 				});
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
+			//Para poder recoger los datos guardados en el local storage al recargar la página
+			getFavourites: () => {
+				setStore({ favourites: JSON.parse(localStorage.getItem("store.favourites")) });
+			},
+
+			deleteFavourite: (itemUrl, favouritesArr) => {
+				favouritesArr.map((item, index) => {
+					if (item.url === itemUrl) {
+						item.favorite = false;
+
+						favouritesArr.splice(index, 1);
+
+						setStore({ favourites: [...favouritesArr] });
+					}
+				});
 			}
 		}
 	};
